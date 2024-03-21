10037e79845c813951475e47537658dfab2362aa
totimehjames123 <totimehjames123@gmail.com>
2024-03-21 03:18:31 +0000

    <a> to <Link />

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import Link from 'next/link'
 
 export default function Hero() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
@@ -100,12 +101,12 @@ export default function Hero() {
             Make sales and record keeping easy and convenient with Epha Medical Pharmacy Inventory System. Say goodbye to stress & paperwork.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
+              <Link
                 href="/login"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Sign in
-              </a>
+              </Link>
               <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
                 Learn more <span aria-hidden="true">→</span>
               </a>
