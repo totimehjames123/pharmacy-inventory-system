825b435df56c51b3c9ffa7cd2af1ebf767a0b161
totimehjames123 <totimehjames123@gmail.com>
2024-03-21 03:10:31 +0000

    worked on hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
+import Hero from "@/Components/Hero";
 
 export default function Home() {
   return (
-    <main className="flex">
-      <h1>Welcome to epha medicals</h1>
-    </main>
+    <>
+      <Hero />
+    </>
   )
 }
